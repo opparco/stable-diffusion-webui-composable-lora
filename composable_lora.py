9f8fdca7b63f3f40a37f524ed0e5a4aef3ff312f
Light-of-Hers <724610746@qq.com>
2023-02-24 19:58:35 +0800

    Fix some bugs: support micro-batched denoising

--- composable_lora.py
+++ composable_lora.py
@@ -23,13 +23,16 @@ def load_prompt_loras(prompt: str):
 
 def reset_text_model_encoder_counter():
     global text_model_encoder_counter
+    global diff_model_layer_counter
 
     # reset counter to uc head
-    text_model_encoder_counter = len(prompt_loras) * 2
+    text_model_encoder_counter = -1
+    diff_model_layer_counter = 0
 
 
 def lora_forward(compvis_module, input, res):
     global text_model_encoder_counter
+    global diff_model_layer_counter
 
     import lora
 
@@ -39,6 +42,10 @@ def lora_forward(compvis_module, input, res):
     lora_layer_name: str | None = getattr(compvis_module, 'lora_layer_name', None)
     if lora_layer_name is None:
         return res
+
+    num_loras = len(lora.loaded_loras)
+    if text_model_encoder_counter == -1:
+        text_model_encoder_counter = len(prompt_loras) * num_loras
 
     # print(f"lora.forward lora_layer_name={lora_layer_name} in.shape={input.shape} res.shape={res.shape}")
 
@@ -63,9 +70,9 @@ def lora_forward(compvis_module, input, res):
                 #
                 # This code evaluated twice in get_learned_conditioning.
                 #
-                if 0 <= text_model_encoder_counter // 2 < len(prompt_loras):
+                if 0 <= text_model_encoder_counter // num_loras < len(prompt_loras):
                     # c
-                    loras = prompt_loras[text_model_encoder_counter // 2]
+                    loras = prompt_loras[text_model_encoder_counter // num_loras]
                     multiplier = loras.get(lora.name, 0.0)
                     if multiplier != 0.0:
                         # print(f"c #{text_model_encoder_counter // 2} lora.name={lora.name} mul={multiplier}")
@@ -79,29 +86,51 @@ def lora_forward(compvis_module, input, res):
                 if lora_layer_name.endswith("_11_mlp_fc2"):  # last
                     text_model_encoder_counter += 1
                     # c1 c1 c2 c2 .. .. uc uc
-                    if text_model_encoder_counter == len(prompt_loras) * 2 + 2:
+                    if text_model_encoder_counter == (len(prompt_loras) + 1) * num_loras:
                         text_model_encoder_counter = 0
 
-            elif res.shape[0] == num_batches * num_prompts + num_batches:  # "diffusion_model_"
-                #
-                #
-                #
-                tensor_off = 0
-                uncond_off = num_batches * num_prompts
-                for b in range(num_batches):
-                    # c
-                    for p, loras in enumerate(prompt_loras):
-                        multiplier = loras.get(lora.name, 0.0)
-                        if multiplier != 0.0:
-                            # print(f"tensor #{b}.{p} lora.name={lora.name} mul={multiplier}")
-                            res[tensor_off] += multiplier * alpha * patch[tensor_off]
-                        tensor_off += 1
+            elif lora_layer_name.startswith("diffusion_model_"): # "diffusion_model_"
 
-                    # uc
-                    if opt_uc_diffusion_model and lora.multiplier != 0.0:
-                        # print(f"uncond lora.name={lora.name} lora.mul={lora.multiplier}")
-                        res[uncond_off] += lora.multiplier * alpha * patch[uncond_off]
-                    uncond_off += 1
+                if res.shape[0] == num_batches * num_prompts + num_batches:
+                    #
+                    #
+                    #
+                    tensor_off = 0
+                    uncond_off = num_batches * num_prompts
+                    for b in range(num_batches):
+                        # c
+                        for p, loras in enumerate(prompt_loras):
+                            multiplier = loras.get(lora.name, 0.0)
+                            if multiplier != 0.0:
+                                print(f"tensor #{b}.{p} lora.name={lora.name} mul={multiplier}")
+                                res[tensor_off] += multiplier * alpha * patch[tensor_off]
+                            tensor_off += 1
+
+                        # uc
+                        if opt_uc_diffusion_model and lora.multiplier != 0.0:
+                            # print(f"uncond lora.name={lora.name} lora.mul={lora.multiplier}")
+                            res[uncond_off] += lora.multiplier * alpha * patch[uncond_off]
+                        uncond_off += 1
+                else:
+                    cur_num_prompts = int(res.shape[0])
+                    base = (diff_model_layer_counter // cur_num_prompts) // num_loras * cur_num_prompts
+                    if 0 <= base < len(prompt_loras):
+                        # c
+                        for off in range(cur_num_prompts):
+                            loras = prompt_loras[base + off]
+                            multiplier = loras.get(lora.name, 0.0)
+                            if multiplier != 0.0:
+                                res[off] += multiplier * alpha * patch[off]
+                    else:
+                        # uc
+                        if opt_uc_diffusion_model and lora.multiplier != 0.0:
+                            res += lora.multiplier * alpha * patch
+
+                    if lora_layer_name.endswith("_output_blocks_11_1_proj_out"):
+                        diff_model_layer_counter += cur_num_prompts
+                        if diff_model_layer_counter >= (len(prompt_loras) + 1) * num_loras:
+                            diff_model_layer_counter = 0
+
             else:
                 # default
                 if lora.multiplier != 0.0:
@@ -131,3 +160,4 @@ opt_uc_diffusion_model = False
 num_batches: int = 0
 prompt_loras: List[Dict[str, float]] = []
 text_model_encoder_counter: int = 0
+diff_model_layer_counter: int = 0
